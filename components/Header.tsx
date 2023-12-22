42cd741627ff6f955c52b38f2a1e9a0f81f1b63b
Franklin <71532004+fran1014@users.noreply.github.com>
2023-12-22 18:54:48 +0100

    created link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
-
+'use client'
 
 function Header() {
   return (
-    <header className="bg-[#013B94]">Header </header>
+    <header className="bg-[#013B94]">
+    <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"arial-label="Global" >
+    
+    <Link></Link>
+    </nav> 
+    </header>
   )
 }
 
