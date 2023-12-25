ce4bea27314bf9bd983224406b5e5547394c168c
Franklin <71532004+fran1014@users.noreply.github.com>
2023-12-25 11:57:42 +0100

    creating nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,6 +32,11 @@ const products = [
         href: "#",
         icon: ChatBubbleLeftIcon,
     }
+];
+
+const callsToAction = [
+    {name: "See Demo Booking", href:"#", icon: PlayCircleIcon},
+    {name: "Contact Support", href:"#", icon: PhoneIcon},
 ]
 
 function Header() {
@@ -76,6 +81,35 @@ function Header() {
             leaveTo="opacity-0 translate-y-1"
             >
                 <Popover.Panel className="absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5">
+                    <div className="p-4">
+                        {products.map((item)=>(
+                            <div
+                            key={item.name}
+                            className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                            >
+                                <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                                    <item.icon
+                                    className="h-6 w-6 text-[#013894] group-hover:text-blue-600"
+                                    aria-hidden="true"
+                                    />
+                                </div>
+                                <div className="auto">
+                                    <a
+                                    href={item.href}
+                                    className="block font-semibold text-[#013894]"
+                                    
+                                    >
+                                        {item.name}
+                                        <span className="absolute inset-0"/>
+                                    </a>
+                                    <p className="mt-1 text-[#013B94]">
+                                        {item.description}
+                                    </p>
+
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 
                 </Popover.Panel>
 
