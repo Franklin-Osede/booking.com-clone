9f4f04fe618667daaa768fdbaa62078a8d3af202
Franklin <71532004+fran1014@users.noreply.github.com>
2023-12-26 21:04:04 +0100

    Created  different page sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,27 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Booking.com clone</h1>
+    <main className="bg-[#013B94]">
+      <section className='max-w-7xl mx-auto p-6'>
+        <h2 className='font-bold text text-5xl text-white'>   Find your Next Stay  </h2>
+          <h3 className='text-white py-5 text-xl'>
+            Search low prices on Hotels, homes and much more...
+
+          </h3>
+   
+      </section>
      
+     <section className='m-4 mt-0 -mb-14 px-2 lg:px-4'>
+      {/*SearchForm */}
+     </section>
+    
+    <section className='mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-lg'>
+    <div className='pt-5'>
+      <h3 className='text-xl font-bold'>Trending Destinations</h3>
+      <p className='font-light'>Most Popular choices for travellers from around the world 
+      </p>
+      </div>
+    </section>
     </main>
   )
 }
